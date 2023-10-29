df231604b1bf9a452d0397ba878639e12ae50408
Nikhil p rao <nikhilprao9066@gmail.com>
2023-10-29 18:18:28 -0400

    forward and backward substitutioon

---
+++ forwardnBack_Substitution/fornbac.py
@@ -0,0 +1,11 @@
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
+L = np.array([[1, 0, 0],
+              [2, 3, 0],
+              [4, 5, 6]])
+
+U = np.array([[1, 2, 3],
+              [0, 4, 5],
+              [0, 0, 6]])
